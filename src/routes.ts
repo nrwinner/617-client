0ccea2eb4e083469df72801884fff770efa82f0c
Nick Winner <design@winnerdigital.net>
2018-05-10 10:18:56 -0400

    Resolve merge conflicts

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const apiBase = 'http://localhost:4000';
-=======
-const apiBase = '';
->>>>>>> 8b5ee22283824f1c48cb7fc0c38e24f6fe49f610
 
 export const api = apiBase + '/api';
 const users = apiBase + '/users'
